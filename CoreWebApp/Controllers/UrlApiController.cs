3b1d781b8622c716ffa6b430b98745d451da4a66
Roel <roelverschuijten@gmail.com>
2018-12-21 16:48:59 +0100

    to less

---
+++ CoreWebApp/Controllers/UrlApiController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreWebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UrlApiController : ControllerBase
+    {
+        public IActionResult Add()
+        {
+            return Ok("Ok!");
+        }
+    }
+}
